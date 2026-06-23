8ad9bd11903c036193e8f3480c686f1f2ff66c5e
driedtoast <driedtoast@gmail.com>
2026-06-23 17:03:28 -0400

    fixing

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -44,7 +44,7 @@ class AppDatabase extends _$AppDatabase {
   AppDatabase.forTesting(QueryExecutor executor) : super(executor);
 
   @override
-  int get schemaVersion => 7;
+  int get schemaVersion => 8;
 
   @override
   MigrationStrategy get migration {
@@ -81,6 +81,10 @@ class AppDatabase extends _$AppDatabase {
         if (from < 7) {
           await m.addColumn(cachedMachines, cachedMachines.uncloudMachineId);
           await m.addColumn(cachedMachines, cachedMachines.uncloudContext);
+        }
+        if (from < 8) {
+          await m.addColumn(cachedMachines, cachedMachines.monthlyCost);
+          await m.addColumn(cachedMachines, cachedMachines.currency);
         }
       },
     );
